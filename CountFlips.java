ab5b6fd32ea5acb50c22de59a9055caac17ea646
DennisD18 <delong@hartford.edu>
2025-03-11 14:16:53 -0400

    Assignment 2 Work

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,52 @@
-public class CountFlips {
-  public static void main(String[] args) {
-  }
+import java.util.Random;
+
+public class CountFlips
+{
+    public static void main(String[] args)
+    {
+        int heads = 0;
+        int tails = 0;
+        int flips = 0;
+        Coin coin = new Coin();
+        while (heads < 100 && tails < 100)
+        {
+            coin.flip();
+            if (coin.isHeads())
+            {
+                heads++;
+                System.out.println("heads");
+            }
+            else
+            {
+                tails++;
+                System.out.println("tails");
+            }
+            flips++;
+        }
+        System.out.println("It took " + flips + " flips to get 100 heads or 100 tails.");
+    }
+}
+
+class Coin {
+    private static final int HEADS = 0;
+    private static final int TAILS = 1;
+    private int face;
+    private Random rand;
+
+    public Coin() {
+        rand = new Random();
+        flip();
+    }
+
+    public void flip() {
+        face = rand.nextInt(2);
+    }
+
+    public boolean isHeads() {
+        return (face == HEADS);
+    }
+
+    public boolean isTails() {
+        return (face == TAILS);
+    }
 }
--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 public class OneHundredBottlesOfBeer {
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int verses;
+
+        // Validate input
+        do {
+            System.out.print("Enter the number of verses to print (1-100): ");
+            while (!scanner.hasNextInt()) {
+                System.out.print("Invalid input. Please enter a number between 1 and 100: ");
+                scanner.next();
+            }
+            verses = scanner.nextInt();
+        } while (verses < 1 || verses > 100);
+
+        // Print the verses
+        for (int i = 100; i > 100 - verses; i--) {
+            System.out.println(i + " bottle" + (i == 1 ? "" : "s") + " of beer on the wall");
+            System.out.println(i + " bottle" + (i == 1 ? "" : "s") + " of beer");
+            System.out.println("If one of those bottles should happen to fall");
+            System.out.println((i - 1) + " bottle" + (i - 1 == 1 ? "" : "s") + " of beer on the wall");
+            System.out.println();
+        }
+
+        scanner.close();
+    }
 }
